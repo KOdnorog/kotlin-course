11a375ee25b59dc0baead3728841d3199df401b3
Ekaterina Odnorog <kodnorog@yandex.ru>
2024-10-06 15:07:20 +0300

    [HW] add solution HW10 task without 10

--- src/main/kotlin/org/eodnorog/kotlincourse/lesson10/Homework10_Array.kt
+++ src/main/kotlin/org/eodnorog/kotlincourse/lesson10/Homework10_Array.kt
@@ -5,7 +5,7 @@ fun main() {
 
 //Задание 1: Создание и Инициализация Массива
 //Создайте массив из 5 целых чисел и инициализируйте его значениями от 1 до 5.
-    val taskHome1Array: Set<Int> = setOf(1, 2, 3, 4, 5)
+    val taskHome1Array: Array<Int> = arrayOf(1, 2, 3, 4, 5)
 
 //Задание 2: Создание Пустого Массива
 //Создайте пустой массив строк размером 10 элементов.
@@ -16,7 +16,7 @@ fun main() {
     val taskHome3Array: Array<Double> = Array(5) { 0.0 }
     for (i in taskHome3Array.indices) {
         taskHome3Array[i] = (i * 2).toDouble()
-        println(taskHome3Array[i])
+        println("Task_3: ${taskHome3Array[i]}")
     }
 
 //Задание 4: Изменение Элементов Массива
@@ -24,43 +24,56 @@ fun main() {
     val taskHome4Array: Array<Int> = Array(5) { 0 }
     for (i in taskHome4Array.indices) {
         taskHome4Array[i] = (i * 3)
-        println(taskHome4Array[i])
+        println("Task - 4: ${taskHome4Array[i]}")
     }
 
 //Задание 5: Работа с Nullable Элементами
 //Создайте массив из 3 nullable строк. Инициализируйте его одним null значением и двумя строками.
     val taskHome5Array: Array<String?> = arrayOfNulls(3)
-    println(taskHome5Array.toList())
+    println("Task_5: ${taskHome5Array.toList()}")
 
 //Задание 6: Копирование Массива
 //Создайте массив целых чисел и скопируйте его в новый массив в цикле.
-    val taskHome6Array: Set<Int> = setOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    val taskHome6Array: Array<Int> = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
     for (i in taskHome6Array) {
         val newArray = setOf(i)
-        println(newArray)
+        println("Task - 6: ${newArray}")
     }
 
 //Задание 7: Разница Двух Массивов
 //Создайте два массива целых чисел одинаковой длины. Создайте третий массив, вычев значения одного из другого.
     val taskHome7ArrayA = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
     val taskHome7ArrayB = arrayOf(9, 8, 7, 6, 5, 4, 3, 2, 1)
-    val newTaskHome7ArrayC = Array(9){0}
+    val newTaskHome7ArrayC = Array(9) { 0 }
     for (i in newTaskHome7ArrayC.indices) {
         newTaskHome7ArrayC[i] = taskHome7ArrayA[i] - taskHome7ArrayB[i]
-        println(newTaskHome7ArrayC[i])
+        println("Task_7: ${newTaskHome7ArrayC[i]}")
     }
 
 //Задание 8: Поиск Индекса Элемента
 //Создайте массив целых чисел. Найдите индекс элемента со значением 5.
 //Если значения 5 нет в массиве, возвращаем -1. Реши задачу через цикл while.
-    val taskHome8Array = setOf(1,2,3,4,5,6,7,8,9,0)
-    println(taskHome8Array)
-
+    val taskHome8Array = arrayOf(1, 2, 3, 4, 9, 6, 8, 8, 7, 0)
+    var count = 0
+    while (count++ < 10) {
+        if (5 in taskHome8Array) {
+            println("Task - 8: ${taskHome8Array.indexOf(5)}")
+            break
+        } else println("Task - 8: -1")
+        break
+    }
 
 //Задание 9: Перебор Массива
 //Создайте массив целых чисел. Используйте цикл для перебора массива и вывода каждого элемента в консоль.
 //Напротив каждого элемента должно быть написано “чётное” или “нечётное”.
-
+    val taskHome9Array = arrayOf(1, 2, 3, 4, 9, 6, 8, 8, 7, 0)
+    for (i in taskHome9Array) {
+        if (i % 2 == 0 && i != 0) {
+            println("$i - чётное")
+        } else if (i == 0) {
+            println("$i - Просто ноль :)")
+        } else println("$i - нечётное")
+    }
 
 //Задание 10: Поиск Значения в Массиве по вхождению
 //Создай функцию, которая принимает массив строк и строку для поиска.
