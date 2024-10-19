0bf2d82532905a91de75b53f7479b65a6896a740
Ekaterina Odnorog <kodnorog@yandex.ru>
2024-10-19 22:33:38 +0300

    [HW] comments HW12

--- src/main/kotlin/org/eodnorog/kotlincourse/lesson12/Homework12.kt
+++ src/main/kotlin/org/eodnorog/kotlincourse/lesson12/Homework12.kt
@@ -1,11 +1,13 @@
 package org.eodnorog.kotlincourse.lesson12
 
 fun main() {
-    noArguments()
-    twoNumbers(5, 2)
-    stringNothing("Nothing")
-    println(srNumbers(listOf(1, 2, 3)))
-    println(numbNullLing("Kelly"))
+//    noArguments()
+//    twoNumbers(5, 2)
+//    stringNothing("Nothing")
+//    println(srNumbers(listOf(1, 2, 3)))
+//    println(numbNullLing("Kelly"))
+//    noArgFNumbers()
+    println(multiplyByTwo(5))
 
 }
 
@@ -62,18 +64,47 @@ fun nullString(a: String?, b: Int?): Boolean? {
 //----- Напиши рабочий код для следующих задач: -----
 //Задача 1:
 //Напишите функцию multiplyByTwo, которая принимает целое число и возвращает его, умноженное на 2.
+fun multiplyByTwo(a: Int) = a * 2
 
 //Задача 2:
 //Создайте функцию isEven, которая принимает целое число и возвращает true, если число чётное, и false в противном случае.
+fun isEven(a: Int): Boolean {
+    if (a % 2 == 0) {
+        return true
+    } else return false
+}
 
 //Задача 3:
 //Напишите функцию printNumbersUntil, которая принимает целое число n и выводит на экран числа от 1 до n.
 //Если число n меньше 1, функция должна прекратить выполнение с помощью return без вывода сообщений.
+fun printNumbersUntil(n: Int) {
+    if (n < 1) return
+    for (i in 1..n) {
+        println(i)
+    }
+}
 
 //Задача 4:
 //Создайте функцию findFirstNegative, которая принимает список целых чисел и возвращает первое отрицательное число в списке.
 //Если отрицательных чисел нет, функция должна вернуть null.
+fun findFirstNegative(a: List<Int>): Int? {
+    for (i in a) {
+        if (i < 0) {
+            return i
+        }
+    }
+    return null
+}
 
 //Задача 5:
 //Напишите функцию processList, которая принимает список строк. Функция должна проходить по списку и выводить каждую строку.
 //Если встречается null значение, функция должна прекратить выполнение с помощью return без возврата значения.
+fun processList(a: List<String?>) {
+    for (i in a) {
+        if (i == null) {
+            return
+        } else {
+            println(i)
+        }
+    }
+}
